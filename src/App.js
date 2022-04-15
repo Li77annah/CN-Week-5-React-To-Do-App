248d4ca9a69672d5014106669d9ccd897a6f3684
Lisa Hawkins <phoenixlah2809@gmail.com>
2022-04-15 11:05:38 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import {useState } from "react"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [tasks, setTasks] = useState([""])
+  const [textInput, setTextInput] = useState("")
 
-export default App;
+  const updateInput = (event) => {
+    setTextInput(event.target.value)
+  }
+
+  const addTask = () => {
+    let addList = [...tasks]
+    addList.push(textInput)
+    setTasks(addList)
+  }
+
+  const deleteTask = (index) => {
+    let deleteList = [...tasks]
+    deleteList.splice(index, 1)
+    setTasks(deleteList)
+  }
+
+  return (
+    <div id="container">
+      <h1 id="title">YOUR TO DO LIST</h1>
+      <h2 id="task1">Task 1: Be Fabulous</h2>
+      <div id="formInput">
+        <input onChange={updateInput}/>
+        <button onClick={addTask}>ADD TASK</button>
+        <button onClick={deleteTask}>DELETE TASK</button>
+        {tasks.map((item, index) => {
+          return <p>{item}</p>
+        })}
+      </div>
+    </div>
+  )
+}
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test("title is rendered", () => {
+  render(<App />)
+  let element = screen.getByText("YOUR TO DO LIST")
+  expect(element).toBeInTheDocument ()
+})
+
+test("Add task button is rendered" , () => {
+  render(<App />)
+  let element = screen.getAllByText("ADD TASK")
+  expect(element.length).toBe(1)
+})
+
+test("Delete task button is rendered" , () => {
+  render(<App />)
+  let element = screen.getAllByText("DELETE TASK")
+  expect(element.length).toBe(1)
+})
+
